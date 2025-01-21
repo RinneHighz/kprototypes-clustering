b4492e545854d28b1e3bd38927f86e82c29f6021
Ruengwith <ruengwith.25.5@gmail.com>
2025-01-21 11:56:39 +0700

    change from decision tree to catboost model

--- app.py
+++ app.py
@@ -18,28 +18,44 @@ def result():
 def predict():
     # รับค่าจาก form
     age = int(request.form['age'])
-    unenconded_gender = request.form['gender']
-    unenconded_education = request.form['education']
+    unencoded_gender = request.form['gender']
+    unencoded_education = request.form['education']
+    unencoded_marital = request.form['marital']
     credit = float(request.form['credit'])
     trans_amt = float(request.form['trans_amt'])
-    income = float(request.form['income'])
+    trans_count = float(request.form['trans_count'])
+    months_on_book = float(request.form['MOB'])
+    min_income = float(request.form['min-income'])
+    max_income = float(request.form['max-income'])
     
     # เข้ารหัสค่าสตริงให้เป็นตัวเลข
-    gender = 1 if unenconded_gender == 'F' else 0
-    education_mapping = {'High School': 0, 'Graduate': 1, 'Uneducated': 2, 'Unknown': 3, 'College': 4, 'Post-Graduate': 5, 'Doctorate': 6}
-    education = education_mapping[unenconded_education]
+    # gender = 1 if unencoded_gender == 'F' else 0
+    
+    gender = unencoded_gender 
+    
+    # education_mapping = {'High School': 0, 'Graduate': 1, 'Uneducated': 2, 'Unknown': 3, 'College': 4, 'Post-Graduate': 5, 'Doctorate': 6}
+    # education = education_mapping[unencoded_education]
+    education = unencoded_education
+    
+    marital = unencoded_marital
     
     # โหลดโมเดลจากไฟล์
-    model = joblib.load('decision_tree_kprototypes_3clusters.pkl')
+    # model = joblib.load('decision_tree_kprototypes_3clusters.pkl')
+    model = joblib.load('catboost_kprototypes_3clusters.pkl')
+
     
     # สร้าง input data สำหรับ prediction พร้อมชื่อคุณลักษณะ
     input_data = {
         'Age': age,
         'Gender': gender,
         'Education_Level': education,
+        'Marital_Status': marital,
+        'Months_on_book': months_on_book,
         'Credit_Limit': credit,
         'Total_Trans_Amt': trans_amt,
-        'Max_income': income,
+        'Total_Trans_Count': trans_count,
+        'Min_income': min_income,
+        'Max_income': max_income,
     }
     
     input_data_df = pd.DataFrame([input_data])
